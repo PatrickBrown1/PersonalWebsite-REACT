412dacc84ce6bc467530b25642c59ef15af0bf9c
PatrickBrown1 <patrickbrown1312@gmail.com>
2020-03-24 13:45:30 -0700

    Button style and hover style completed

--- src/NavBar.js
+++ src/NavBar.js
@@ -6,11 +6,8 @@ export default class NavBar extends React.Component {
     render() {
         return(
             <div className='NavBar'>
-                <p>This is my nav bar! lol not yet but we're getting there</p>
-                <div className='NavBar-buttons'>
-                    <CustomNavButton className='NavBar-onebutton'>Link 1</CustomNavButton>
-                    <CustomNavButton className='NavBar-onebutton'>Link 2</CustomNavButton>
-                </div>
+                <CustomNavButton className='NavBar-onebutton'>Link 1</CustomNavButton>
+                <CustomNavButton className='NavBar-onebutton'>Link 2</CustomNavButton>
             </div>
         );
     }
