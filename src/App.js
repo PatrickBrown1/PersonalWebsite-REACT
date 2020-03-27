469439508421a8a908690ba9930c15918ab1b866
PatrickBrown1 <patrickbrown1312@gmail.com>
2020-03-27 15:02:55 -0700

    Made title text, revamped navbar. Rerenders when the page isn't at the top. WHY DOESNT EXPAND ONCLICK WORK?

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import React from 'react';
 import './App.css';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
 import NavBar from'./NavBar.js';
 import Background from'./Background.js';
-function App() {
-  return (
-    <div className="App">
-      <NavBar/>
-      <Background/>
-      
-      <header className="App-header">
-        <p className="Title">Patrick Brown's Personal Website!</p>
-      </header>
-      <body className="App-body">
-        <p>Make header first because why not</p>
-        <p2>Make the header normal for over 50% window size</p2>
-        <p2>Then make it a square button three lines if it's under 50%</p2>
-      </body>
-    </div>
-  );
+
+
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      top: true
+    }
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  } 
+  componentWillUnmount() { 
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+  handleScroll() {
+    if(window.pageYOffset <= 0){
+      this.setState({top: true});
+    }
+    else{
+      this.setState({top: false});
+    }
+  }
+  
+  
+  render() {
+    return (
+      <div className="App" ref="appref" onScroll={this.handleScroll}>
+        <NavBar atTop={this.state.top}/>
+        <Background/>
+
+        <div >
+          <header className="App-header">
+            <p className="Title">Patrick Brown's Personal Website!</p>
+          </header>
+          <div className="App-body">
+            <p>Make header first because why not</p>
+            <p>Make the header normal for over 50% window size</p>
+            <p>Then make it a square button three lines if it's under 50%</p>
+            <p>As soon as the screen is not at the top, change navbar to a circle three lines button on right</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+
+ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.unregister();
--- src/NavBar.js
+++ src/NavBar.js
@@ -3,22 +3,73 @@ import './NavBar.css';
 import {Button, ExpandingButton}  from './CustomNavButton.js'
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 export default class NavBar extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            atTop: true,
+            expandedNav: false
+        };
+        this.handleExpandClick = this.handleExpandClick.bind(this);
+    }
+    componentWillReceiveProps(nextProps){
+        this.updateState();
+    }
+    handleExpandClick(){
+        alert("expand navbar");
+        this.setState({expandedNav: true});
+    }
+    updateState(){
+        this.setState({atTop: this.props.atTop});
+        if(this.state.atTop = true){
+            this.setState({expandedNav: false});
+        }
+    }
     render() {
+        
         return(
-            <div className='NavBar'>
-                <Button>Link 1</Button>
-                <Button>Link 2</Button>
-                <ExpandingButton
-                    redirect="https://github.com/PatrickBrown1">
-                    <FaGithub size={32} style={{verticalAlign: 'middle'}}/>
-                    &thinsp; GitHub
-                    
-                </ExpandingButton>
-                <ExpandingButton
-                    redirect="https://www.linkedin.com/in/P-Brown">
-                    <FaLinkedin size={32} style={{verticalAlign: 'middle'}}/>
-                    &thinsp; LinkedIn
-                </ExpandingButton>
+            <div>
+                {this.state.atTop ? (
+                    <div key="a" className='NavBar'>
+                        <Button>Link 1</Button>
+                        <Button>Link 2</Button>
+                        <ExpandingButton
+                            redirect="https://github.com/PatrickBrown1">
+                            <FaGithub size={32} style={{verticalAlign: 'middle'}}/>
+                            &thinsp; GitHub
+                        </ExpandingButton>
+                        <ExpandingButton
+                            redirect="https://www.linkedin.com/in/P-Brown">
+                            <FaLinkedin size={32} style={{verticalAlign: 'middle'}}/>
+                            &thinsp; LinkedIn
+                        </ExpandingButton>
+                    </div>
+                ) : [
+                    this.state.expandedNav ? (
+                        <div key="b" className='NavBar'>
+                            <Button>Link 9</Button>
+                            <Button>Link 2</Button>
+                            <ExpandingButton
+                                redirect="https://github.com/PatrickBrown1">
+                                <FaGithub size={32} style={{verticalAlign: 'middle'}}/>
+                                &thinsp; GitHub
+                                
+                            </ExpandingButton>
+                            <ExpandingButton
+                                redirect="https://www.linkedin.com/in/P-Brown">
+                                <FaLinkedin size={32} style={{verticalAlign: 'middle'}}/>
+                                &thinsp; LinkedIn
+                            </ExpandingButton>
+                        </div>
+                    ) : (
+                        <div key="c" className='NavBar'>
+                            <Button onClick={ () => {this.handleExpandClick()} }>
+                                Expand
+                            </Button>
+                            
+                        </div>
+                    )
+                ]}
+            
             </div>
         );
     }
